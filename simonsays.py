3e0759365a4ee56473168db0cd4806dfe46c1b77
mxggie <maggieschwierking@gmail.com>
2018-07-24 15:40:52 -0500

    adds random colors to color sequence with sounds

--- simonsays.py
+++ simonsays.py
@@ -7,20 +7,35 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 colors = [ 'R' , 'G' , 'B' , 'Y' ]
+freq = [220, 440, 880, 1760]
+
 R_pin = 11
 G_pin = 12
 B_pin = 13
 LED.setup(R_pin, G_pin, B_pin)
+buzz_pin = 32
+
+GPIO.setup(buzz_pin,GPIO.OUT)
+Buzz = GPIO.PWM(buzz_pin, 1000)
 
 def blink_lights(): 
-    for i in range(0, len(colors)):
-        n = random.randint(0,3)
-        colors.append(colors[n])
-#        color_string = ''.join(colors)            
-        LED.setColor(colors[n])
-        time.sleep(0.5)
-        LED.noColor()
-        time.sleep(0.5)
+    n = random.randint(0,3)    
+    color_sequence = [colors[n]]
+    freq_sequence = [freq[n]]
+    while True:
+        for i in range(0, len(color_sequence)):             
+            LED.setColor(color_sequence[i])
+            time.sleep(0.5)
+            Buzz.ChangeFrequency(freq_sequence[i])        
+            Buzz.start(50)
+            time.sleep(0.5)
+            Buzz.stop()
+            LED.noColor()
+            time.sleep(0.5)
+        n = random.randint(0,3)            
+        color_sequence.append(colors[n])
+        freq_sequence.append(freq[n])
+        time.sleep(1)
 
 if __name__ == '__main__':
     try:
